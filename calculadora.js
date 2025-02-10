d7fcc57c139eb46f2f6ed3f107186a5b4749a339
Christian <christisidro0516@gmail.com>
2025-02-10 11:17:03 +0100

    calc cd

--- calculadora.js
+++ calculadora.js
@@ -6,6 +6,7 @@ function multiplicar(a, b) { return a * b; }
 
 function dividir(a, b) { if (b === 0) { throw new Error("No se puede dividir por cero"); } return a / b; }
 
+function raiz(a) { return Math.sqrt(a); }
 // Hacer las funciones accesibles en el navegador 
 
 module.exports = { suma, resta, multiplicar, dividir };

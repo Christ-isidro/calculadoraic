757fe3791b35e62a7cb475b5511c06310ff0220f
Christian <christisidro0516@gmail.com>
2025-02-10 11:01:00 +0100

    calculadora cd

--- calculadora.js
+++ calculadora.js
@@ -1,22 +1,12 @@
-function sumar(a, b) {
-    return a + b;
-}
+function suma(a, b) { return a + b; }
 
-function restar(a, b) {
-    return a - b;
-}
+function resta(a, b) { return a - b; }
 
-function multiplicar(a, b) {
-    return a * b;
-}
+function multiplicar(a, b) { return a * b; }
 
-function dividir(a, b) {
-    return a / b;
-}
-
-function raiz(c) {
-    return Math.sqrt(c);
-}
-
-
-module.exports = { sumar, restar, multiplicar, dividir, raiz };   
+function dividir(a, b) { if (b === 0) { throw new Error("No se puede dividir por cero"); } return a / b; }
+// Hacer las funciones accesibles en el navegador 
+window.suma = suma;
+window.resta = resta;
+window.multiplicar = multiplicar;
+window.dividir = dividir;
